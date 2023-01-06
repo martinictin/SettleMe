2850320aad29de740706dd0b2898eb329caf244d
Neviox <martinicoding@gmail.com>
2023-01-06 17:41:39 +0100

    firebase authentication init setup

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 import { Navigation } from "./src/infrastructure/navigation";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import {
   useFonts as useOswald,
@@ -14,7 +16,41 @@ import { ProductsContextProvider } from "./src/services/products/products.contex
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAqBlxP35uEtc6Vb-GxPIqDe4HMo0MAAuc",
+
+  authDomain: "settleme-e4d99.firebaseapp.com",
+
+  projectId: "settleme-e4d99",
+
+  storageBucket: "settleme-e4d99.appspot.com",
+
+  messagingSenderId: "219423936459",
+
+  appId: "1:219423936459:web:3e1147bf7bc55f5268f04e",
+};
+
+const app = initializeApp(firebaseConfig);
+export const authentication = getAuth(app);
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(
+        authentication,
+        "martinicoding@gmail.com",
+        "testtest"
+      )
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -24,6 +60,10 @@ export default function App() {
   });
 
   if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
     return null;
   }
 
